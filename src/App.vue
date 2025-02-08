ede992e62c8ed920035f7e80da86ca17de7c4ed5
bmartin042503 <bmartin042503@gmail.com>
2025-02-08 23:50:05 +0100

    cf logo

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,19 @@
 
 <template>
   <main class="container">
-    
+    <img src="/catflower-logo.svg"/>
   </main>
 </template>
+
+<style>
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+img {
+  height: 10em;
+}
+</style>
